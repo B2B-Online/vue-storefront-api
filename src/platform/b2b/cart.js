ab94da5dd3c713f467e39ae9a7b3ebd8cb318ddb
Czeslaw Szczotka <czeslaw.szczotka@grandparade.co.uk>
2020-01-11 20:35:40 +0100

    b2b cart api integration

--- src/platform/b2b/cart.js
+++ src/platform/b2b/cart.js
@@ -1,28 +1,176 @@
 
 import AbstractCartProxy from '../abstract/cart'
 import { multiStoreConfig } from './util'
-
+import rp from 'request-promise-native';
+import e, { response } from 'express';
+/**
+ * Specification https://github.com/DivanteLtd/storefront-integration-sdk/blob/tutorial/Dynamic%20API%20specification.md
+ */
 class CartProxy extends AbstractCartProxy {
     constructor (config, req) {    
-        super(config, req)
+      super(config, req)
+      this.config = require('config');
+      this.redis = require('redis');
     }
 
-    create (customerToken) {
-      return new Promise((resolve, reject) => {
-        resolve([]);
-      })
+    saveInRedis (sessionId, cartId) {
+      let redisClient = this.redis.createClient(this.config.redis);
+      redisClient.on('error', function (err) {
+        redisClient = this.redis.createClient(this.config.redis);
+      });
+
+      if (this.config.redis.auth) {
+        redisClient.auth(this.config.redis.auth);
+      }
+
+      redisClient.set("cart_" + cartId, JSON.stringify({
+        session: sessionId,
+        created_at: new Date(),
+      }));
     }
-  
-    /**
-     * 
-     *   {"code":200,
-     * "result":{"item_id":7604,"sku":"WS09-XS-Blue","qty":1,"name":"Tiffany Fitness Tee","price":28,"product_type":"configurable","quote_id":"11228","product_option":{"extension_attributes":{"configurable_item_options":[{"option_id":"93","option_value":50},{"option_id":"136","option_value":167}]}}}}     
-    */
-    update (customerToken, cartId, cartItem) {
+
+    findSession(cartId, successCallback, errorCallback) {
+      let redisClient = this.redis.createClient(this.config.redis);
+      redisClient.on('error', function (err) {
+        redisClient = this.redis.createClient(this.config.redis);
+      });
+      
+      if (this.config.redis.auth) {
+        redisClient.auth(this.config.redis.auth);
+      }
+      const key = "cart_" + cartId;
+      redisClient.get(key, (err, value) => {
+        if(value) {
+          successCallback(value);
+        } else {
+          errorCallback(err);
+        }
+      });
+    }
+
+    findSessionWrapper(cartId) {
+      const that = this;
       return new Promise((resolve, reject) => {
+        that.findSession(cartId, (successResponse) => {
+              resolve(successResponse);
+          }, (errorResponse) => {
+              reject(errorResponse)
+          });
+      });
+   }
+
+    create (customerToken) {
+      const options = {
+        uri: 'https://cartapi.systemb2b.pl/api/get_or_create_cart/gci/1078',
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        //json: true,
+        //timeout: 15000,
+        qs: {
+          //user_id : "9b06dc1de20040a6822dabbd29797b1c",
+          format: "json",
+          title: "default",
+          //session_key: "test123"
+        }
+     };
+     
+     if(customerToken) {
+       options.qs.user_id = customerToken;
+     } else {
+       const uuid = require('uuid');
+       options.qs.session_key = uuid.v1();
+     } 
+     const that = this;
+     return  rp(options)
+     .then(function (resp) {
+        const json = JSON.parse(resp);         
+       that.saveInRedis(json.session, json.cart_id);   
+       return new Promise((resolve, reject) => {
+         resolve(json.cart_id);
+       })
+     })
+     .catch(function (err) {
+       console.error('Error during call: b2bapieu.planetb2b.com/api/product/symbol/', err);  
+       reject();     
+     }); 
+    }
+    
+
+   async pull (token, cartId, params) {
+    const options = {
+      uri: 'https://cartapi.systemb2b.pl/api/get_or_create_cart/gci/1078',
+      method: 'GET',
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true,
+      //timeout: 15000,
+      qs: {         
+        cache: false,
+        format: "json",
+        title: "default",         
+      }
+    };
+
+    if(!token) {               
+       const res = await this.findSessionWrapper(cartId);
+       options.qs.session_key = JSON.parse(res).session;
+    } else {
+      options.qs.user_id = customerToken;
+    }
+    
+    return  rp(options).then(function (resp) {      
+      const basket = [];
+      resp.products.forEach(item => {
+        basket.push({
+          //"item_id": 66257,
+          //"sku": "WS08-M-Black",
+          //"qty": 1,
+          //"name": "Minerva LumaTech&trade; V-Tee",
+          //"price": 32,
+          //"product_type": "configurable",
+          //"quote_id": "dceac8e2172a1ff0cfba24d757653257",    
+          //"product_option": { }
+        })
+      });               
+      return new Promise((resolve, reject) => {
+        resolve(basket);
+      })
+    }).catch(function (err) {
+      console.error('Error during call: b2bapieu.planetb2b.com/api/product/symbol/', err);  
+      reject();     
+    }); 
+
+  }
+
+  update (customerToken, cartId, cartItem) {
+    const options = {
+      uri: 'https://cartapi.systemb2b.pl/api/get_or_create_cart/gci/1078',
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true,
+      //timeout: 15000,
+      qs: {       
+        format: "json",
+        title: "default",
+      }
+   };
+
+   if(!token) {               
+     const res = await this.findSessionWrapper(cartId);
+     options.qs.session_key = JSON.parse(res).session;
+   } else {
+     options.qs.user_id = customerToken;
+   }
+   //product_id=311340&quantity=1&customer_code=&variant_id=252245
+    return new Promise((resolve, reject) => {
         resolve([]);
       })        
-    }
+  }
   
     applyCoupon (customerToken, cartId, coupon) {
       return new Promise((resolve, reject) => {
@@ -44,21 +192,15 @@ class CartProxy extends AbstractCartProxy {
   
 
     delete (customerToken, cartId, cartItem) {
-        return new Promise((resolve, reject) => {
+        
+      return new Promise((resolve, reject) => {
             resolve({
                 "code": 200,
                 "result": cartId
               });
-          })     
-    }
-   /**
-    * {"code":200,"result":[]}
-    */
-    pull (customerToken, cartId, params) {
-    return new Promise((resolve, reject) => {
-       resolve([]);
       })     
     }
+
   
     totals (customerToken, cartId, params) {
       return new Promise((resolve, reject) => {
@@ -189,6 +331,4 @@ class CartProxy extends AbstractCartProxy {
         });
     }
 }
-
-
 module.exports = CartProxy;
